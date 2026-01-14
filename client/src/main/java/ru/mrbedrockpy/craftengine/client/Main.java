1a902b0a83c698f211cedce778c0ebee5c19acd1
mrbedrockpy <MrBedrockpy@yandex.ru>
2026-01-14 16:14:29 +0300

    plans for project in main class

--- client/src/main/java/ru/mrbedrockpy/craftengine/client/Main.java
+++ client/src/main/java/ru/mrbedrockpy/craftengine/client/Main.java
@@ -2,11 +2,15 @@ package ru.mrbedrockpy.craftengine.client;
 
 public class Main {
 
+    // TODO: сделать нормальное освещение
+    // TODO: сделать систему Entity: ИИ, рендеринг и тд
+    // TODO: Сделать поддержку Generic моделей из BlockBench
+
     public static void main(String[] args) {
         try {
             CraftEngineClient.INSTANCE.run();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
